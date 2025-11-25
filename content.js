75103f0cb813f79d77e7751c5665faf41175097d
KaiSingL <laut9810@gmail.com>
2025-11-26 00:58:27 +0800

    Fix the expand icon

--- content.js
+++ content.js
@@ -266,12 +266,12 @@ function addPromptListButton() {
 				</svg>
 			`;
 			const expandIcon = `
-				<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="transform: rotate(180deg);">
+				<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 					<path d="M3 5h8"/>
 					<path d="M3 12h8"/>
 					<path d="M3 19h8"/>
-					<path d="m15 5 3 3 3-3"/>
-					<path d="m15 19 3-3 3 3"/>
+					<path d="m15 8 3-3 3 3"/>
+					<path d="m15 16 3 3 3-3"/>
 				</svg>
 			`;
 
